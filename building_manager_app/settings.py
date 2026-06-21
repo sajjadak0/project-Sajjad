eab67b516b24f2458432bdb31f35161138c52aee
sajjadak0 <akbarisajjad999@gmail.com>
2026-06-21 07:05:55 -0700

    add create magic link and _send_reset_email service

--- building_manager_app/settings.py
+++ building_manager_app/settings.py
@@ -144,7 +144,8 @@ if DEBUG:
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = os.environ.get("EMAIL_HOST")
-    EMAIL_PORT = os.environ.get("EMAIL_PORT")
-    EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+    EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+    EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "False") == "True"
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+    DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
--- core/services/email_services.py
+++ core/services/email_services.py
@@ -1,4 +1,6 @@
 import secrets
+from django.core.mail import send_mail
+from django.conf import settings
 from datetime import timedelta, datetime
 from django.utils import timezone
 
@@ -11,7 +13,23 @@ def _token_generator() -> tuple[str, datetime]:
     return token, expires_at
 
 
+def _send_reset_email(email: str, token: str) -> None:
+    reset_link = f"http://localhost:5173/reset-password?token={token}"
+    subject = "Reset your password"
+    message = f"""
+    Click the link below to reset your password:
+
+    {reset_link}
+
+    This link will expire in 15 minutes.
+    """
+    send_mail(
+        subject, message, settings.DEFAULT_FROM_EMAIL, [email], fail_silently=False
+    )
+
+
 def create_magic_link(user: UserModel) -> str:
     token, expires_at = _token_generator()
     ResetPassword.objects.create(user=user, token=token, expires_at=expires_at)
+    _send_reset_email(user.email, token)
     return token
