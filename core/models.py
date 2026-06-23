d9ae0306728309bf01f4eb9be0489e85d6e78cf3
SAO311-83 <arman830606@gmail.com>
2026-06-23 23:11:42 +0330

    userModel setup completed

--- core/models.py
+++ core/models.py
@@ -5,5 +5,13 @@ from django.db import models
 class UserModel(AbstractUser):
     USERNAME_FIELD = "email"
     email = models.EmailField(max_length=256, unique=True)
-    username = models.CharField(max_length=256)
+    username = models.CharField(max_length=256, blank=True)
     REQUIRED_FIELDS = []
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
